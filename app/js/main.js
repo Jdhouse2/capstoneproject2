679b6395c6263914745844aeb7da48fb7f551eae
thelmle <thelmle@asu.edu>
2019-04-09 16:04:58 -0700

    Added POC functionality and announcements sidebar. Will need to fix the css of announcements to fit in the sidebar when expanded.

--- app/js/main.js
+++ app/js/main.js
@@ -34,6 +34,7 @@ function checkAuth() {
     } else {
         console.log(localStorage.getItem("ownerid") + " is signed in");
         console.log(localStorage.getItem("modDist") + " is mod status");
+        console.log(localStorage.getItem("pocDist") + " is poc status");
         // hide irrelevant nav links
         document.querySelectorAll("a[href='EATLogon.html']")[0].style.display = "none";
         // document.querySelectorAll("a[href='signup.html']")[0].style.display = "none";
